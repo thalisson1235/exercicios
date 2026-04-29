8093dd4458f17080586dc8e7011a6ca1f2dfc639
thalisson <thalisson1240@gmail.com>
2026-04-29 18:35:25 -0300

    ja eu termino

--- ex01.java
+++ ex01.java
@@ -1,5 +1,13 @@
+import java.util.Scanner;
+
 public class ex01 {
     public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Digite o seu nome: ");
+        String nome = sc.nextLine();
+        sc.close();
         
     }
 }
