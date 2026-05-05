6a702ebe5f1ddfea9a713eb5d97141db3c76d33c
th <thalisson1240@gmail.com>
2026-05-05 20:29:43 -0300

    ex02

--- ex01.java
+++ ex01.java
@@ -2,16 +2,34 @@ import java.util.Scanner;
 
 public class ex01 {
     public static void main(String[] args) {
-
+        int quantiddfilhos = 0;
         Scanner sc = new Scanner(System.in);
 
         System.out.println("Digite o seu nome: ");
         String nome = sc.nextLine();
         
 
-        System.out.println("qual seu números de horas trabalhadas? ");
+        System.out.println("Quantas horas voce trabalhou? ");
         int hrstrabalhadas = sc.nextInt();
-        sc.close();
+
+        System.out.println("Quanto voce recebe por hora? ");
+        double recebeporhrs = sc.nextDouble();
+        sc.nextLine();
+
+        System.out.println("Voce tem filhos?");
+        String filhos = sc.nextLine().toLowerCase();
+
+        if (filhos.equals("sim") ) {
+            System.out.println("Quantos filhos voce tem? ");
+            quantiddfilhos = sc.nextInt();
+               
+        }
+
+        double calcHrs = hrstrabalhadas * recebeporhrs;
+ 
+        double salariofinal = calcHrs * (1 + ( 0.03 * quantiddfilhos));
         
+        System.out.println("Olá, "+nome+" voce tem o salario de R$: "+salariofinal);
+        sc.close();
     }
 }
---
+++ ex02.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class ex02 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        double quantidademax, quantidademin, quantidademedia;
+
+        System.out.println("qual a quantidade máxima de estoque? ");
+        quantidademax = sc.nextDouble();
+        sc.nextLine();
+        
+        System.out.println("Qual a quantidade mínima de estoque? ");
+        quantidademin = sc.nextDouble();
+        sc.nextLine();
+
+        quantidademedia = (quantidademax + quantidademin)/2;
+
+        System.out.println("A quantidade média do estoque da peça é de: "+quantidademedia);
+        sc.close();
+
+    }
+}
